4438a585f7c9a4a6a690bedf134c2b0f0bf3deee
iwuqing <1069243171@qq.com>
2018-10-09 00:29:26 +0800

    finish
    初步完成版本，具有搜索、下载、可视化等功能。

--- cugWeb/home/views.py
+++ cugWeb/home/views.py
@@ -7,10 +7,14 @@ import json
 
 
 def home(request):
+    '''主页'''
     timeline, ah_value, nh_value = [],[],[]
     min_ah,min_nh,max_ah,max_nh = [],[],[],[]
+    filedate = None
+    #   收到表单提交
     if request.POST:
         filedate = request.POST['filedate']
+        #   搜索框未输入
         if filedate == '':
             return render(request, 'home.html',
                           {
@@ -29,12 +33,13 @@ def home(request):
                           )
         timeline, ah_value, nh_value = find(
                 r"SELECT create_data,receive_msg FROM gprs_receive WHERE create_data LIKE '"+filedate+r"%:00'")
+        #   未检索到数据文件
         if len(timeline) == 0:
             return render(request, 'home.html',
                           {
                               'input_warning':'alert alert-danger alert-dismissible',
                               'close_button':'X',
-                              'information':'错误！未检索到日期或输入日期格式错误。',
+                              'information':'错误！未检索到数据或输入日期格式错误。',
                               'timeline': json.dumps(timeline),
                               'ah_value': json.dumps(ah_value),
                               'nh_value': json.dumps(nh_value),
@@ -45,6 +50,7 @@ def home(request):
                               'current_time': str(datetime.date.today()),
                           }
                           )
+        #   成功检索到数据文件
         if len(timeline) != 0:
             min_ah = min(ah_value)
             max_ah = max(ah_value)
@@ -52,6 +58,9 @@ def home(request):
             max_nh = max(nh_value)
             return render(request, 'home.html',
                               {
+                                  'input_warning': 'alert alert-success alert-dismissible',
+                                  'close_button': 'X',
+                                  'information': '恭喜！成功检索数据并完成数据可视化。',
                                   'timeline': json.dumps(timeline),
                                   'ah_value': json.dumps(ah_value),
                                   'nh_value': json.dumps(nh_value),
@@ -62,6 +71,7 @@ def home(request):
                                   'current_time': str(datetime.date.today()),
                               }
                           )
+    #   正常启动或刷新home
     return render(request, 'home.html',
                   {
                       'timeline': json.dumps(timeline),
@@ -74,81 +84,33 @@ def home(request):
                       'current_time': str(datetime.date.today()),
                   }
                   )
-# Create your views here.
-# def home(request):
-#     timeline, ah_value, nh_value = find(
-#         r"SELECT create_data,receive_msg FROM gprs_receive WHERE create_data LIKE '%:00'")
-#     current_time = datetime.date.today()
-#     min_ah = min(ah_value)
-#     max_ah = max(ah_value)
-#     min_nh = min(nh_value)
-#     max_nh = max(nh_value)
-#     if request.POST:
-#         filedate = request.POST['filedate']
-#     return render(request, 'home.html',
-#                       {
-#                           'current_time':str(current_time),
-#                           'timeline':json.dumps(timeline),
-#                           'ah_value':json.dumps(ah_value),
-#                           'nh_value':json.dumps(nh_value),
-#                           'min_ah':json.dumps(min_ah),
-#                           'max_ah':json.dumps(max_ah),
-#                           'min_nh':json.dumps(min_nh),
-#                           'max_nh':json.dumps(max_nh),
-#                       }
-#                   )
-
-def data(request):
-    # if request.POST:
-    #     filedate = request.POST['filedate']
-    timeline, ah_value, nh_value = find(
-            r"SELECT create_data,receive_msg FROM gprs_receive WHERE create_data LIKE 'fwa'")
-    current_time = datetime.date.today()
-    # min_ah = min(ah_value)
-    # max_ah = max(ah_value)
-    # min_nh = min(nh_value)
-    # max_nh = max(nh_value)
-    min_ah = 0
-    max_ah = 0
-    min_nh = 0
-    max_nh = 0
-    return render(request, 'data.html',
-                      {
-                          'current_time':str(current_time),
-                          'timeline':json.dumps(timeline),
-                          'ah_value':json.dumps(ah_value),
-                          'nh_value':json.dumps(nh_value),
-                          'min_ah':json.dumps(min_ah),
-                          'max_ah':json.dumps(max_ah),
-                          'min_nh':json.dumps(min_nh),
-                          'max_nh':json.dumps(max_nh),
-                      }
-                  )
-
 
 def download(request):
-    return render(request, 'download.html')
-
-def d2ownload(request):
-    '''数据下载'''
+    '''数据下载页面'''
     timeline, ah_value, nh_value = [],[],[]
     filedate = None
     if request.POST:
-        #   接收文件日期，并检索数据 bv
         filedate = request.POST['filedate']
+        #   未输入日期
         if filedate == '':
             return render(request, 'download.html',
                           {
-                              'input_info': '请输入需要下载的数据所在日期（例如：2016-06-07）'
+                              'input_warning':'alert alert-danger alert-dismissible',
+                              'close_button':'X',
+                              'information':'错误！未输入数据所在日期。',
+                              'current_time': str(datetime.date.today()),
                           }
                           )
         timeline, ah_value, nh_value = find(
-            r"SELECT create_data,receive_msg FROM gprs_receive WHERE create_data LIKE '"+filedate+r"%'")
+            r"SELECT create_data,receive_msg FROM gprs_receive WHERE create_data LIKE '" + filedate + r"%'")
+        #   未检索到数据
         if len(timeline) == 0:
-            #   未检索到数据时
             return render(request, 'download.html',
                           {
-                              'input_info': '数据不存在或输入日期格式错误'
+                              'input_warning': 'alert alert-danger alert-dismissible',
+                              'close_button': 'X',
+                              'information': '错误！未检索到数据或输入日期格式错误。',
+                              'current_time': str(datetime.date.today()),
                           }
                           )
         if len(timeline) != 0:
@@ -171,11 +133,10 @@ def d2ownload(request):
             return response
 
     return render(request, 'download.html',
-                    {
-                        'input_info': '请输入需要下载的数据所在日期（例如：2016-06-07）'
-                    }
+                  {
+                      'current_time': str(datetime.date.today()),
+                  }
                   )
-
 
 def find(commond):
     db = pymysql.connect('123.207.7.51', 'root', 'root', 'lsq_gprs')  # 连接对象
